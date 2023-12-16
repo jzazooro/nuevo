29169484fc39818776583aaf006987895cc472c9
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-12-16 12:54:45 +0100

    inicio de sesion reciclado y probado

---
+++ usuario.py
@@ -0,0 +1,71 @@
+import csv
+import os
+
+def correo_existe(correo):
+    if not os.path.isfile('clientes.csv'):
+        return False
+
+    with open('clientes.csv', 'r') as archivo:
+        reader = csv.reader(archivo)
+        next(reader)  # Saltar encabezados
+        for fila in reader:
+            if fila[0] == correo:
+                return True
+        return False
+
+def registrar_usuario():
+    correo = input("Ingrese su correo electrónico: ")
+    if correo_existe(correo):
+        print("Este correo electrónico ya está registrado. Por favor, use otro.")
+        return
+
+    contraseña = input("Ingrese su contraseña: ")
+    direccion = input("Ingrese su dirección: ")
+    guardar_usuario(correo, contraseña, direccion)
+    print("Usuario registrado exitosamente.")
+
+def guardar_usuario(correo, contraseña, direccion):
+    existe_archivo = os.path.isfile('clientes.csv')
+    with open('clientes.csv', 'a', newline='') as archivo:
+        writer = csv.writer(archivo)
+        if not existe_archivo:
+            writer.writerow(['Correo', 'Contraseña', 'Dirección'])  # Encabezados
+        writer.writerow([correo, contraseña, direccion])
+
+def iniciar_sesion():
+    if not os.path.isfile('clientes.csv'):
+        print("No hay usuarios registrados.")
+        return
+
+    correo = input("Ingrese su correo electrónico: ")
+    contraseña = input("Ingrese su contraseña: ")
+    if verificar_usuario(correo, contraseña):
+        print("Inicio de sesión exitoso.")
+    else:
+        print("Correo o contraseña incorrectos, o el usuario no existe.")
+
+def verificar_usuario(correo, contraseña):
+    with open('clientes.csv', 'r') as archivo:
+        reader = csv.reader(archivo)
+        next(reader)  # Saltar encabezados
+        for fila in reader:
+            if fila[0] == correo and fila[1] == contraseña:
+                return True
+        return False
+
+# Menú principal
+while True:
+    print("\nBienvenido a la Pizzería")
+    print("1. Registrarse")
+    print("2. Iniciar Sesión")
+    print("3. Salir")
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == '1':
+        registrar_usuario()
+    elif opcion == '2':
+        iniciar_sesion()
+    elif opcion == '3':
+        break
+    else:
+        print("Opción no válida.")
