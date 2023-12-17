7393faa0fe35504c1d85f95e0d6f890231355cce
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-12-17 13:02:25 +0100

    importaciones

--- ejercicio2/lanzador2.py
+++
@@ -1,7 +0,0 @@
-from prueba import *
-from prueba2 import *
-from prueba3 import *
-from prueba4 import *
-
-def main():
-    print("Ejercicio 1")
---
+++ lanzador2.py
@@ -0,0 +1,10 @@
+# importa todo lo que hay en la carpeta ejercicio2
+
+from ejercicio2.prueba import *
+from ejercicio2.prueba2 import *
+from ejercicio2.prueba3 import *
+from ejercicio2.prueba4 import *
+ 
+
+def main():
+    print("Ejercicio 1")
